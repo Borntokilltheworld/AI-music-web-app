9a21d8f93d220e61985d7b36860201174e99ad1f
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-01-21 13:44:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+song1 = "";
+song2 = "";
 function setup(){
     video = createCapture(VIDEO);
    video.hide()
@@ -6,9 +8,10 @@ function setup(){
     canvas.center()
 }
 function draw(){
-    image(video,0,0,600,500)
+    image(video,0,0,600,500);
 }
 function preload(){
-
+song1 = loadSound("The Avengers Theme Song.mp3");
+song2 = loadSound("CWs The Flash.mp3");
 }
     
