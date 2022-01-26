4644f4bff97105b6bd5e50eb7f84a3f536cdb491
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-01-26 13:32:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
 
     video = createCapture(VIDEO);
-   video.hide()
+    video.hide();
     
-    canvas = createCanvas(600,500);
-    canvas.center()
+    posenet = ml5.poseNet(video,ModelLoaded);
+    posenet.on('pose',gotPoses);
+}
+function ModelLoaded(){
+    console.log('Posenet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX , "leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX,"rightWristY = "+rightWristY);
+    }
 }
 function draw(){
     image(video,0,0,600,500);
@@ -15,4 +36,10 @@ function preload(){
 song1 = loadSound("The Avengers Theme Song.mp3");
 song2 = loadSound("CWs The Flash.mp3");
 }
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
     
